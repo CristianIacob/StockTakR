d5074dfdc3fbe9773bcc84905da90fecf7bbb6c0
Cristian Iacob <Cristian-Ioan.Iacob@haufe-lexware.com>
2016-10-22 00:34:44 +0300

    Impletented functionality for adding a stock, using an alert prompt.

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,55 +1,75 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
 
-
-// import * as cxml from 'cxml';
-// import * as example from 'cxml/test/xmlns/dir-example';
-
-
 @Component({
-  templateUrl: 'list.html'
+    templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+    selectedItem: any;
+    icons: string[];
+    items: Array<{ title: string, icon: string }>;
 
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+        // If we navigated to this page, we will have an item available as a nav param
+        this.selectedItem = navParams.get('item');
 
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+        this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+            'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Stock ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+        this.items = [];
+
+        for (let i = 1; i < 6; i++) {
+            this.items.push({
+                title: 'Stock ' + i,
+                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+            });
+        }
     }
 
+    itemTapped(event, item) {
+        this.navCtrl.push(ItemDetailsPage, {
+            item: item
+        });
+    }
 
+    addStock(event) {
+        this.presentPrompt();
+    }
 
-    // var parser = new cxml.Parser();
-    // var result = parser.parse('<dir name="empty"></dir>', example.document);
-
-    // result.then((doc: example.document) => {
-    //     console.log(JSON.stringify(doc));
-    // });
-
-  }
-
-  itemTapped(event, item) {
-   alert("test");
-    this.navCtrl.push(ItemDetailsPage, {
-      item: item
-    });
-  }
+    presentPrompt() {
+        let alert = this.alertCtrl.create({
+            title: 'Add stock',
+            inputs: [
+                {
+                    name: 'stockName',
+                    placeholder: 'Stock name'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Add',
+                    role: 'Add',
+                    handler: data => {
+                        this.items.push({
+                            title: data.stockName,
+                            icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+                        });
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
 }
